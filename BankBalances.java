9d3e20222df5b42bafc912c1fd21fcb38a3fef69
Scott <scottgeilen99@gmail.com>
2020-03-07 20:26:19 -0500

    fixed noob method issues

--- BankBalances.java
+++ BankBalances.java
@@ -10,107 +10,89 @@ public class BankBalances {
     public static void main(String[] args) {
         Date today = Calendar.getInstance().getTime();
         System.out.println("Today: " + today);
+        //restart = false;
         menu();
-
     }
     private static void menu() {
-        System.out.println("WELCOME TO SCOTTBANK");
-        System.out.println("1 - Add purchase");
-        System.out.println("2 - Calculate a paycheck");
-        
         Scanner scan = new Scanner(System.in);
-        Integer menu = scan.nextInt();
-        switch (menu) {
-            case 1:
-                Spend();
-            case 2:
-                calculatePaycheck();
+        try {
+            System.out.println("WELCOME TO SCOTTBANK");
+            System.out.println("1 - Add purchase");
+            System.out.println("2 - Calculate a paycheck");
+            System.out.println("3 - Exit");
+            
+            
+            Integer menu = scan.nextInt();
+            switch (menu) {
+                case 1:
+                    Spend();
+                    break;
+                case 2:
+                    calculatePaycheck();
+                    break;
+                case 3:
+                    break;
+            }
+        }
+        finally {
+            if (scan != null)
+                scan.close();
         }
     }
 
     private static String calculatePaycheck() {
-        DecimalFormat df = new DecimalFormat("#.00");
-        String payFormatted = "";
         Scanner scan = new Scanner(System.in);
-        System.out.println("Would you like to calculate a paycheck? y/n");
-        String response = scan.nextLine();
-        switch (response) {
-            case "y":
-                System.out.println("Hours: ");
-                double hours = scan.nextDouble();
+        try {
+            DecimalFormat df = new DecimalFormat("#.00");
+            String payFormatted = "";
+            System.out.println("Hours: ");
+            double hours = scan.nextDouble();
 
-                System.out.println("Wage: ");
-                double wage = scan.nextDouble();
+            System.out.println("Wage: ");
+            double wage = scan.nextDouble();
 
-                double taxes = 0.165;
-                double taxedWage = taxes * 18.00;
-                double newWage = wage - taxedWage;
-                double toSavings = 100.00;
-                double pay = hours * newWage - toSavings;
-                payFormatted = df.format(pay);
-                System.out.println("Paycheck: " + payFormatted);
-                break;
-            case "n":
-                System.out.println("Okay.");
-                break;
+            double taxes = 0.165;
+            double taxedWage = taxes * 18.00;
+            double newWage = wage - taxedWage;
+            double toSavings = 100.00;
+            double pay = hours * newWage - toSavings;
+            payFormatted = df.format(pay);
+            System.out.println("Paycheck: " + payFormatted);
+
+            scan.close();
+            return payFormatted;
         }
-        return payFormatted;
+        finally {
+            if (scan != null)
+                scan.close();
+        }
+        
     }
-    // private static String StartChecking(double startCheckingBalance) {
-    //     DecimalFormat df = new DecimalFormat("#.00");
-    //     String newStartingBalance = df.format(startCheckingBalance);
-    //     return newStartingBalance;
-    // }
-
-
-
-
-    // private static String EndChecking(double endCheckingBalance) {
-    //     Double EndBalance = StartChecking(1544.32) - Spend(100.00);
-    //     DecimalFormat df = new DecimalFormat("#.00");
-    //     String newEndBalance = df.format(endCheckingBalance);
-    //     return newEndBalance;
-    // }
-    // public int getCurrCheckBal() {
-    //     return StartChecking - Spend;
-    // }
     private static Double Spend() {
         ArrayList<Double> spendAmount = new ArrayList<Double>();
+        //Scanner scan = new Scanner(System.in);
         Scanner scan = new Scanner(System.in);
-        double sum = 0;
-        System.out.println("Would you like to add purchases? y/n ");
-        String response = scan.nextLine();
-        switch (response) {
-            case "y":
-                System.out.println("How many purchases would you like to add?");
-                int NoOfPurchases = scan.nextInt();
-                for (int i=0; i<NoOfPurchases; i++) {
-                    System.out.print("How much was your purchase? ");
-                    Double currPur = scan.nextDouble();
-                    spendAmount.add(currPur);
-                }
-                break;
-            case "n":
-                System.out.println("Okay.");
-                break;
-        }
-        for (Double purchases : spendAmount) {
-                sum += purchases;
-        }
-        System.out.println(sum);
-        return sum;
+        try {
+            double sum = 0;
+            System.out.println("How many purchases would you like to add?");
+            int NoOfPurchases = scan.nextInt();
+            for (int i=0; i<NoOfPurchases; i++) {
+                System.out.print("How much was your purchase? ");
+                Double currPur = scan.nextDouble();
+                spendAmount.add(currPur);
+            }
+            for (Double purchases : spendAmount) {
+                    sum += purchases;
+            }
 
-        // DecimalFormat df = new DecimalFormat("#.00");
-        // String newStartingBalance = df.format(startCheckingBalance);
-        // return newSpendAmount;
+
+
+            System.out.println(sum);
+            return sum;
+        }
+        finally {
+            if (scan != null)
+                scan.close();
+        }
     }
-    // public double addPurchases() {
-    //     double sum = 0;
-    //     for (int i=0; i<NoOfPurchases; i++) {
-    //         System.out.print("How much was your purchase? ");
-    //         Double currPur = scan.nextDouble();
-    //         spendAmount.add(currPur);
-    //     }
-    //     return sum;
-    // }
 }
